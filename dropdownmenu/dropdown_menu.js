2fed7b5e6990c1018f8ba1d054f801fe1522aa12
Joe Prempeh <prempehjoe@gmail.com>
2015-08-29 15:32:03 -0700

    work done

--- dropdownmenu/dropdown_menu.js
+++ dropdownmenu/dropdown_menu.js
@@ -39,11 +39,44 @@ var MENU = [
   }
 ];
 
+
 $(document).ready(function () {
-	// loop over each menu function
-	$.each(MENU, function (el, index) {
+	// cache jQuery selectors
+	$menu = $('.menu');
 
+	// make functions
+	function createSubmenu(item, element) {
+		console.log(item);
+		console.log(element);
+		// create a menu item to hold our menu
+		var $menuItem = $('<div class="menu-item" />');
+		var $menuName = $('<span class="menu-name">');
+		$menuName.text(item.title).appendTo($menuItem);
+		$menu.append($menuItem);
+	}
+
+	function createMenu(item) {
+		console.log(item);
+		// create a menu item to hold our menu
+		var $menuItem = $('<div class="menu-item" />');
+		var $menuName = $('<span class="menu-name">');
+		$menuName.text(item.title).appendTo($menuItem);
+
+		// if the menu has a submenu, add the event listener
+		if (item.submenu) {
+			console.log('has submenu')
+			$menuItem.on('hover')
+		}
+		$menu.append($menuItem);
+
+	}
+
+	// loop over each menu item and make a cell
+	$.each(MENU, function (index, element) {
+		createMenu(element);
+		// if the menu has a submenu, display it on hover
+		if(element.submenu) {
+			createSubmenu(element.submenu)
+		}
 	});
-
-	$('.menu-item')
 });
